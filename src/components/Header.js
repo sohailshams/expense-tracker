f0807d760ba543b882274f77047b3e43d1a41cfd
sohailshams <sohailshamas@yahoo.com>
2021-01-03 15:47:19 +0000

    Created Header component and added materialize css link

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper">
+          <h2 className="header">Expense Tracker</h2>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
