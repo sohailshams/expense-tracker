da1a667dbdd18b92e271fea735bf708f0cda5b3b
sohailshams <sohailshamas@yahoo.com>
2021-01-04 21:55:36 +0000

    Updated Balance component - added GlobalContext and rendered total dynamically

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const transactionAmounts = transactions.map(
+    (transaction) => transaction.transactionAmount
+  );
+  const total = transactionAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   return (
     <div>
       <div className="current-balance">
         <h4>Current Balance</h4>
-        <h4>$0.00</h4>
+        <h4 className={total < 0 ? 'minus-color' : 'plus-color'}>
+          ${Math.abs(total)}
+        </h4>
       </div>
     </div>
   );
