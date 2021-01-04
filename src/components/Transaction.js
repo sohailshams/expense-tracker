2319e9fbea5999410dfcbff7bcc13874d6cd0c9b
sohailshams <sohailshamas@yahoo.com>
2021-01-04 20:08:08 +0000

    Added if statement to dynamicaly add plus and minus sign and respective hover color

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Transaction = ({ transaction }) => {
+  const sign = transaction.transactionAmount < 0 ? '-' : '+';
   return (
-    <li className="plus">
+    <li className={transaction.transactionAmount < 0 ? 'minus' : 'plus'}>
       {transaction.description}
-      <span>{transaction.transactionAmount}</span>
+      <span>
+        {sign}${Math.abs(transaction.transactionAmount)}
+      </span>
       <button className="delete-btn">X</button>
     </li>
   );
