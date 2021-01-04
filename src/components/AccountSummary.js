c53369ab3592289a0c64407e201acf1b8acebc4f
sohailshams <sohailshamas@yahoo.com>
2021-01-04 22:14:50 +0000

    Updated AccountSummary component - added functionality to calculate income and expense

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const AccountSummary = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const transactionAmounts = transactions.map(
+    (transaction) => transaction.transactionAmount
+  );
+
+  const income = transactionAmounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense =
+    transactionAmounts
+      .filter((item) => item < 0)
+      .reduce((acc, item) => (acc += item), 0) * -(1).toFixed(2);
   return (
     <div className="account-summary">
       <div className="income">
         <h5>Income</h5>
-        <p className="income-expense green-text text-darken-4">$0.00</p>
+        <p className="income-expense green-text text-darken-4">${income}</p>
       </div>
 
       <div className="expense">
         <h5>Expense</h5>
-        <p className="income-expense red-text text-darken-4">$0.00</p>
+        <p className="income-expense red-text text-darken-4">${expense}</p>
       </div>
     </div>
   );
